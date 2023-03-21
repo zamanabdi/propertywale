b32d415cfab7ac7b6af48cb7aff369e92f3f33ee
zamanabdi <zaman.abdi24@gmail.com>
2023-03-22 00:38:42 +0530

    Changes made to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,20 +39,20 @@ const Header = () => {
     >
       <header className="flex  md:h-[56px] flex-row w-[1550px] justify-between items-center px-3">
         <div
-          className={"hover:cursor-pointer md:ml-[45px]"}
+          className={"hover:cursor-pointer"}
           style={{ display: "flex", flexDirection: "row" }}
           onClick={() => navigate("/")}
         >
           <img src={Logo} className="h-7 w-10  md:w-[75px] md:h-[50px]" />
-          <span className="flex justify-center items-center text-orange-400 text-[15px] md:text-[2rem] font-bold shrink-0 font-" style={{fontFamily:'Abril Fatface'}}>
+          <span className="flex justify-center items-center text-orange-400 text-[12px] sm:text-[15px] md:text-[2rem] font-bold shrink-0" style={{fontFamily:'Abril Fatface'}}>
             Property
           </span>
-          <span className="flex items-center justify-center text-gray-500 text-[15px] md:text-[2rem] font-bold shrink-0" style={{fontFamily:'Abril Fatface'}}>
+          <span className="flex items-center justify-center text-gray-500 text-[12px] sm:text-[15px] md:text-[2rem] font-bold shrink-0" style={{fontFamily:'Abril Fatface'}}>
             Wale
           </span>
         </div>
         <div className="flex flex-row md:w-[500px] h-full items-center justify-center">
-          <ul className="flex bg-white md:flex-row justify-evenly items-center space-x-3 md:space-x-20 text-[14px] md:text-[1.5rem] font-semibold text-gray-500">
+          <ul className="flex bg-white md:flex-row justify-evenly items-center space-x-3 md:gap-10 text-[14px] md:text-[1.5rem] font-semibold text-gray-500 ml-5">
             <Link className={`${pathMatchRoute("/") && "text-black md:border-b-[4px] border-b-[3px] border-b-orange-600 md:border-b-orange-600"} py-[10px] md:py-[8px]`} to={"/"}>Home</Link>
             <Link className={`${pathMatchRoute("/offers") && "text-black md:border-b-[4px] border-b-[3px] border-b-orange-600 md:border-b-orange-600"} py-[10px] md:py-[8px]`} to={"/offers"}>
               <li>Offers</li>
